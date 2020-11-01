4250af468a463c4fc9a91baf27f1bec7f1587004
adam-91 <hadalaadam@gmail.com>
2020-11-01 17:12:07 +0100

    Sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,24 +2,27 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([row,fieldData]) => {
-      res.render('shop/product-list', {
-        prods: row,
-        pageTitle: 'All Products',
-        path: '/products'
-      });
-    })
-    .catch(err => console.log(err));
+  Product.findAll()
+  .then(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findByPk(prodId)
+    .then(product => {
       console.log(product);
+      console.log('test');
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products'
       });
@@ -28,14 +31,16 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('shop/index', {
-        prods: rows,
-        pageTitle: 'Shop',
-        path: '/'
-    })
-    .catch(err => console.log(err));
+  Product.findAll()
+  .then(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  })
+  .catch(err => {
+    console.log(err);
   });
 };
 
--- util/database.js
+++ util/database.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complite',
-    password: 'rp2009'
+const sequelize = new Sequelize('node-complite','root','rp2009', {
+    dialect: 'mysql', 
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+//const mysql = require('mysql2');
+
+//const pool = mysql.createPool({
+//    host: 'localhost',
+//    user: 'root',
+//   database: 'node-complite',
+//    password: 'rp2009'
+//});
+
+//module.exports = pool.promise();
