52e1860248565f28b8991cebd5817b0b61f7c56a
adam-91 <hadalaadam@gmail.com>
2020-11-01 19:40:22 +0100

    working wersion

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -37,12 +41,18 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsToMany(Product, { through: CartItem});
+Product.belongsToMany(Cart, { through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem});
 
 sequelize
     .sync()
+    //.sync( {force: true})
     .then(result => {
         return User.findByPk(1);
-        //app.listen(3000);
     })
     .then(user => {
         if(!user) {
@@ -51,6 +61,9 @@ sequelize
         return user;
     })
     .then(user => {
+        return user.createCart();
+    })
+    .then(cart => {
         app.listen(3000);
     })
     .catch(err => {
